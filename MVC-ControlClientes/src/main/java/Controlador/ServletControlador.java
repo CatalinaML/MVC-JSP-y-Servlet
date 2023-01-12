a259bf4947690fe0b02cf399119637b7a0bf3f07
CatalinaML <listecatalina7@gmail.com>
2023-01-12 15:39:07 -0300

    Update ServletControlador.java
    cambio de alcance de variables (no reenvía el formulario si actualiza la página)

--- MVC-ControlClientes/src/main/java/Controlador/ServletControlador.java
+++ MVC-ControlClientes/src/main/java/Controlador/ServletControlador.java
@@ -14,16 +14,54 @@ public class ServletControlador extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Cliente> clientes = new ClienteDAO().listar();
-        
-        req.setAttribute("clientes", clientes);
-        req.setAttribute("totalClientes", clientes.size());
-        req.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));
-        req.getRequestDispatcher("clientes.jsp").forward(req, resp);
+        this.accionDefault(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String accion = req.getParameter("accion");
+        if(accion != null){
+            switch(accion){
+                case "insertar":
+                    this.insertarCliente(req, resp);
+                    break;
+                    default:
+                        this.accionDefault(req, resp);
+                        break;
+            }
+        }else{
+            this.accionDefault(req, resp);
+        }
+    }
+    
+    private void accionDefault(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        List<Cliente> clientes = new ClienteDAO().listar();
+        HttpSession sesion = req.getSession();
+        sesion.setAttribute("clientes", clientes);
+        sesion.setAttribute("totalClientes", clientes.size());
+        sesion.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));
+        //req.getRequestDispatcher("clientes.jsp").forward(req, resp);
+        resp.sendRedirect("clientes.jsp"); //redirigir notificando al navegador (no se reenvian los formularios
+    }
+    
+    private void insertarCliente (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        
+        String nombre = req.getParameter("nombre");
+        String apellido = req.getParameter("apellido");
+        String email = req.getParameter("email");
+        String telefono = req.getParameter("telefono");
+        String saldoString = req.getParameter("saldo");
+        double saldo = 0;
+        if(saldoString != null && !saldoString.equals("")){
+            saldo = Double.parseDouble(saldoString);
+        }
+        
+        Cliente nuevo = new Cliente(nombre, apellido, email, telefono, saldo);
+        
+        int registrosModificados = new ClienteDAO().insertar(nuevo);
+        System.out.println("registrosModificados = " + registrosModificados);
+        
+        this.accionDefault(req, resp);
         
     }
     
